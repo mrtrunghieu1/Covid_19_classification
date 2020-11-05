a8cf561e286dd395d7c15ea2e8e1e3e4b93b6836
Hieu Vu <mrtrunghieu1>
2020-11-05 15:57:03 +0700

    [UPDATE] Code

--- demo_covid_detection.py
+++ demo_covid_detection.py
@@ -6,14 +6,15 @@ import re
 import PIL
 import torch
 import timm
-
+import argparse
 from torchvision import transforms, models, datasets
 from randaugment import RandAugment, ImageNetPolicy, Cutout
 import torch.nn as nn
 # My packages
 
 from data_helper import data_dir, CHECK_POINT_PATH
-from training import create_model
+from training import create_model, train_model
+from utils import load_model
 
 def main(args):
     ''' Main function for 
@@ -28,7 +29,9 @@ def main(args):
     '''
 
     # Parameters
-    batch_size = args.batch_size 
+    batch_size = args.batch_size
+    epochs = args.epochs
+
     flag_model = args.flag_model
     # Define your transforms for the training and testing sets
     data_transforms = {
@@ -73,18 +76,19 @@ def main(args):
         param.requires_grad = True
 
     # Configs some last layers for my tasks classification covid
-    fc = nn.Sequential(OrderedDict([('fc1', nn.Linear(2048, 1000, bias=True)),
-							     ('BN1', nn.BatchNorm2d(1000, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
-								 ('dropout1', nn.Dropout(0.7)),
-                                 ('fc2', nn.Linear(1000, 512)),
-								 ('BN2', nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
-								 ('swish1', Swish()),
-								 ('dropout2', nn.Dropout(0.5)),
-								 ('fc3', nn.Linear(512, 128)),
-								 ('BN3', nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
-							     ('swish2', Swish()),
-								 ('fc4', nn.Linear(128, 3)),
-								 ('output', nn.Softmax(dim=1))
+    fc = nn.Sequential(OrderedDict([
+                                ('fc1', nn.Linear(2048, 1000, bias=True)),
+                                ('BN1', nn.BatchNorm2d(1000, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
+                                ('dropout1', nn.Dropout(0.7)),
+                                ('fc2', nn.Linear(1000, 512)),
+                                ('BN2', nn.BatchNorm2d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
+                                ('swish1', Swish()),
+                                ('dropout2', nn.Dropout(0.5)),
+                                ('fc3', nn.Linear(512, 128)),
+                                ('BN3', nn.BatchNorm2d(128, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)),
+                                ('swish2', Swish()),
+                                ('fc4', nn.Linear(128, 3)),
+                                ('output', nn.Softmax(dim=1))
 							 ]))
 
     # Connect pretrained model with modified classifer layer
@@ -109,5 +113,75 @@ def main(args):
         print("checkpoint not found")
 
     if checkpoint == None:
+        CHECK_POINT_PATH = CHECK_POINT_PATH
+        
+    model, best_val_loss, best_val_acc = train_model(model, criterion, optimizer, scheduler, num_epochs=epochs, checkpoint=torch.load(CHECK_POINT_PATH))
+    torch.save({'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': optimizer.state_dict(),
+            'best_val_loss': best_val_loss,
+            'best_val_accuracy': best_val_acc,
+            'scheduler_state_dict': scheduler.state_dict(),
+            }, CHECK_POINT_PATH)
     
+    # Load checkpoint path 
+    try:
+        checkpoint = torch.load(CHECK_POINT_PATH)
+        print("checkpoint loaded")
+    except:
+        checkpoint = None
+        print("checkpoint not found")
 
+    load_model(model= model, checkpoint= checkpoint, path=CHECK_POINT_PATH)
+
+
+    # Testing
+    since = time.time()
+    model.eval()
+    y_test = []
+    y_pred = []
+    for images, labels in data_loader['test']:
+        images = Variable(images.cuda())
+        labels = Variable(labels.cuda())
+        outputs = model(images)
+        _, predictions = outputs.max(1)
+        
+        y_test.append(labels.data.cpu().numpy())
+        y_pred.append(predictions.data.cpu().numpy())
+        
+    y_test = np.concatenate(y_test)
+    y_pred = np.concatenate(y_pred)
+    pd.DataFrame({'true_label':y_test,'predicted_label':y_pred}).to_csv('Modified_EfficienNet_B0_Covid-19_Test.csv', index=False)
+    time_elapsed = time.time() - since
+
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    sns.heatmap(confusion_matrix(y_test, y_pred))
+    accuracy_score(y_test, y_pred)
+
+    report = classification_report(y_test, y_pred)
+    print(report)   
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--batch_size',
+        help='batch_size',
+        default=16,
+        type=int
+    )
+    parser.add_argument(
+        '--epochs',
+        help='epochs',
+        default=30,
+        type=int
+    )
+    parser.add_argument(
+        '--flag_model',
+        help='flag_model',
+        default=0,
+        type=int
+    )
+
+    args = parser.parse_args()
+    # Call main function
+    main(args)
--- utils.py
+++ utils.py
@@ -1,8 +1,6 @@
 
 
 
-
-
 #### to show some  image
 def showimage(data_loader, number_images, cat_to_name):
     dataiter = iter(data_loader)
@@ -15,6 +13,15 @@ def showimage(data_loader, number_images, cat_to_name):
         img = np.transpose(images[idx])
         plt.imshow(img)
         ax.set_title(cat_to_name[labels.tolist()[idx]])
+
+
+def load_model(model, checkpoint, path):                                
+    model.load_state_dict(checkpoint['model_state_dict'])
+    best_model_wts = copy.deepcopy(model.state_dict())
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
+    best_loss = checkpoint['best_val_loss']
+    best_acc = checkpoint['best_val_accuracy']
 
 
         
